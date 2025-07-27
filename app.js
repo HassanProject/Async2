a5ac6220251cd40b0ace017d0543d77ca9b2e174
Hassan Kamara <hasfahk4@gmail.com>
2025-07-27 10:48:58 +0000

    setup http request with Async for multiple customers from a arrary of json objects

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // Event Listener on btn1
 document.getElementById('btn1').addEventListener('click', loadCustomer);
 
+// Event Listener on btn2
+document.getElementById('btn2').addEventListener('click', loadCustomers);
+
+// Function for Single customer
 function loadCustomer(e) {
     // Create XHR Object
     const xhr = new XMLHttpRequest();
@@ -20,6 +24,38 @@ function loadCustomer(e) {
             </ul>`;
 
             document.getElementById('customer').innerHTML = output;
+            // console.log(this.responseText);
+        }
+    }
+    xhr.send();
+    // console.log('customer');
+}
+
+// Function for Multiple customers
+function loadCustomers(e) {
+    // Create XHR Object
+    const xhr = new XMLHttpRequest();
+    // Open Request
+    xhr.open('GET', 'customers.json',true);
+    // Load Request
+    xhr.onload = function(){
+        if (this.status === 200) {
+
+            const customers = JSON.parse(this.responseText);
+
+             let output ='';
+
+           customers.forEach(function(customer){
+             output += `
+            <ul>
+                <li>${customer.id}</li>
+                <li>${customer.name}</li>
+                <li>${customer.company}</li>
+                <li>${customer.phone}</li>
+            </ul>`;
+           })
+
+            document.getElementById('customers').innerHTML = output;
             // console.log(this.responseText);
         }
     }
